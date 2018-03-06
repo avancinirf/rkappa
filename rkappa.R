b97191a591ad96ab1aebc879caec7f3f8bdbe4ee
Ricardo Avancini <avancini.rf@gmail.com>
2018-03-06 01:41:38 +0000

    iniciando o projeto

---
+++ rkappa.R
@@ -0,0 +1,8 @@
+
+### ---------------------------------------------------------------------------
+### Título: Função para gerar Matriz de Confusão e calcular índice kappa
+### Autor: Ricardo Avancini
+### e-mail: avancini.rf@gmail.com
+### Repositório: https://github.com/avancinirf/rkappa
+### ---------------------------------------------------------------------------
+
